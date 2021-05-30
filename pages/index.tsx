28c7e0b5a5aaf7b062f066f17231949aada7cbf0
Gerson Groth <ge.groth@gmail.com>
2021-05-30 20:36:12 -0300

    fix build

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Layout from '../components/layout';
 import MoreStories from '../components/more-stories';
 import { getAllPostsForHome, getPage } from '../lib/api';
 
-export default function Index({ allPosts, preview, page }) {
+export default function Index({ allPosts, preview, page }: any) {
     console.log(page);
     const heroPost = allPosts[0];
     const morePosts = allPosts.slice(1);
